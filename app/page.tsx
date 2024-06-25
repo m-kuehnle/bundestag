4c5b5a3470c6ed59a0d9516011aaa62792acc0ac
Leo17220310 <125191929+Leo17220310@users.noreply.github.com>
2024-06-25 19:35:05 +0200

    for pull

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,30 @@ export default async function Home() {
   const person: Person = await fetchPerson();
 
   return (
-    <div className="">Hallo Welt!</div>
-   
+    <div>
+      <h1 className='text-2xl font-bold mb-4'>Willkommen im Bundestag Dashboard</h1>
+      <div>
+        <h2>Personendetails:</h2>
+        <p><strong>ID:</strong> {person.id}</p>
+        <p><strong>Name:</strong> {person.vorname} {person.nachname}</p>
+        <p><strong>Titel:</strong> {person.titel}</p>
+        <p><strong>Typ:</strong> {person.typ}</p>
+        <p><strong>Wahlperiode:</strong> {person.wahlperiode}</p>
+        <p><strong>Aktualisiert am:</strong> {new Date(person.aktualisiert).toLocaleDateString('de-DE')}</p>
+        <p><strong>Datum:</strong> {new Date(person.datum).toLocaleDateString('de-DE')}</p>
+        <p><strong>Basisdatum:</strong> {new Date(person.basisdatum).toLocaleDateString('de-DE')}</p>
+        <h3>Rollen:</h3>
+        <ul> 
+          {person.person_roles.map((role, index) => (
+            <li key={index}>
+              <p><strong>Funktion:</strong> {role.funktion}</p>
+              <p><strong>Zusatzfunktion:</strong> {role.funktionszusatz}</p>
+              <p><strong>Bundesland:</strong> {role.bundesland}</p>
+              <p><strong>Wahlperioden Nummer:</strong> {role.wahlperiode_nummer.join(', ')}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }

24fb0b92d36fca7f1f14efad7d80a5d0035dfa9e
Manuel Kühnle <ge36sej@mytum.de>
2024-06-29 13:50:48 +0200

    add table for Bundestagsmitglieder

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,15 @@ import {
   AllPersons,
   fetchAllPersons,
 } from "../lib/api";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 export default async function Home() {
   const protocol: Protocol = await fetchProtocol();
@@ -24,7 +33,9 @@ export default async function Home() {
 
   return (
     <div className="p-4">
-      <h1 className="text-4xl font-bold mb-4 text-center">Deutscher Bundestag</h1>
+      <h1 className="text-4xl font-bold mb-4 text-center">
+        Deutscher Bundestag
+      </h1>
       <div className="flex justify-center mt-4">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 max-w-screen-lg px-4">
           <DashboardCard numFound={numPersons} title={"Personen"} />
@@ -39,23 +50,34 @@ export default async function Home() {
         <p>Text: {protocol.documents[0].text}</p>
 
         <h3 className="text-3xl font-bold my-8">Bundestagsmitglieder:</h3>
-        <div>
-          {allPersons.documents.map((person, index) => (
-            <div key={index} className="border p-4 mb-4">
-              <p>{index}</p>
-              <p className="font-bold">
-                {" "}
-                {person.vorname} {person.nachname}
-              </p>
 
-              <p>Position: {parseTitle(person.titel).position}</p>
-              <p>Fraktion: {parseTitle(person.titel).fraktion}</p>
-              <p>Wahlperiode: {person.wahlperiode}</p>
-            </div>
-          ))}
-        </div>
-
-        <p></p>
+        <Table>
+          <TableCaption>Bundestagsmitglieder</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]"></TableHead>
+              <TableHead className="w-[300px]">Name</TableHead>
+              <TableHead>Position</TableHead>
+              <TableHead>Fraktion</TableHead>
+              <TableHead className="text-right">Wahlperiode</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {allPersons.documents.map((person, index) => (
+              <TableRow key={index}>
+                <TableCell className="font-medium">{index + 1}</TableCell>
+                <TableCell className="font-medium">
+                  {person.vorname} {person.nachname}
+                </TableCell>
+                <TableCell>{parseTitle(person.titel).position}</TableCell>
+                <TableCell>{parseTitle(person.titel).fraktion}</TableCell>
+                <TableCell className="text-right">
+                  {person.wahlperiode}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </div>
     </div>
   );
